a30957483eeb4d138645de3a6352f9151571df0e
Derek Dinsmore <DinsmoreDesign@users.noreply.github.com>
2018-05-08 11:28:35 -0700

    Commiting reproducable error.

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+
+  <div id="app">
+    
+    <vue-autonumeric
+      id="inputToEmpty"
+      options="euro"
+      v-model="emptyValue"
+      placeholder="Enter your value here"
+    />
+
+  </div>
+
+</template>
+
+
+
+
+<script>
+
+  import VueAutonumeric from 'vue-autonumeric';
+
+  export default {
+
+    name: 'app',
+    components: {
+			VueAutonumeric
+		},
+    data () {
+      return {
+        emptyValue: ''
+      }
+    }
+
+  }
+
+</script>
+
+
+
+
+<style lang="scss">
+
+
+
+</style>
